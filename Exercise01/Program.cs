c2d9aba7da37a411dc3fb01938adc1ec48732997
amiirhoseinsi <amirhoseinsaeedi54655465@gmail.com>
2023-05-09 22:25:15 +0330

    adding Exercise01

---
+++ Exercise01/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM",
+    "ABU DHABI", "PARIS" };
+string[] sortedCities = cities.OrderBy(c => c.Length).ToArray();
+foreach (string c in sortedCities)
+{
+    Console.WriteLine(c);
+}
